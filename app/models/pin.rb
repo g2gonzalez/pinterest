4a071a5d0fba4dcab0ab77bcb1845544dda3e9b5
Gerardo Gonzalez <gonzalezrgerardo@gmail.com>
2015-01-07 22:24:32 -0800

    Updated styling for all pages

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	
-	has_attached_file :image, :styles => { :medium => "350x350>", :thumb => "100x100>" }
-	do_not_validate_attachment_file_type :image
+	has_attached_file :image, :styles => { :medium => "700x700>", :thumb => "100x100>" }
+	validates_attachment :image, :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/gif", "image/png"] }
 end
